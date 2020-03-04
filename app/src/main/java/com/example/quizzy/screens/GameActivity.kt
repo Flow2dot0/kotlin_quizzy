671e26e6c9fecc52c414b886c7b990e819c831b2
Flow2dot0 <florian.gustin@edu.itescia.fr>
2020-03-04 21:47:27 +0100

    feat : adding youtube videos working, implementing path into firebase db

--- app/src/main/java/com/example/quizzy/screens/GameActivity.kt
+++ app/src/main/java/com/example/quizzy/screens/GameActivity.kt
@@ -1,15 +1,22 @@
 package com.example.quizzy.screens
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.quizzy.R
 import com.example.quizzy.models.MyManager
 import kotlinx.android.synthetic.main.activity_game.*
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerSupportFragment
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(), YouTubePlayer.OnInitializedListener {
 
     val manager = MyManager()
+
+    private val RECOVERY_DIALOG_REQUEST = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +25,13 @@ class GameActivity : AppCompatActivity() {
         var isDone = View.INVISIBLE
 
         card2.visibility = isDone
+        val youTubePlayerFragment = supportFragmentManager.findFragmentById(R.id.third_party_player_view) as YouTubePlayerSupportFragment?
+        youTubePlayerFragment?.initialize("AIzaSyB5hIzmoI7JANpXYWQLm4liboActq_VXUQ", this)
 
         // TODO : display value for each radio button
 
-        var playSoundButton = playSoundButton.setOnClickListener {
 
-        }
+
 
 
         // TODO : get value of selected radio if OK pressed
@@ -37,5 +45,25 @@ class GameActivity : AppCompatActivity() {
         // TODO : at the last question navigate to ResultsActivity
         // TODO : then after 3 seconds navigate to HomeActivity
 
+    }
+
+    // On success
+    override fun onInitializationSuccess(provider: YouTubePlayer.Provider,youTubePlayer: YouTubePlayer,wasRestored: Boolean) {
+        if (!wasRestored) {
+            youTubePlayer.cueVideo("-WzHcCfZwRs")
+        }
+    }
+
+    // On error
+    override fun onInitializationFailure(provider: YouTubePlayer.Provider,youTubeInitializationResult: YouTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError) {
+            youTubeInitializationResult.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show()
+        } else {
+            val errorMessage = String.format(
+                "There was an error initializing the YouTubePlayer (%1\$s)",
+                youTubeInitializationResult.toString()
+            )
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+        }
     }
 }
