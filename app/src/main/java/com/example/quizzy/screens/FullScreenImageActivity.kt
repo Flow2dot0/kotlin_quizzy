5d44249b9160aafa849d9314b4c44301449f02ec
Flow2dot0 <florian.gustin@edu.itescia.fr>
2020-03-05 22:47:52 +0100

    style : style full screen

---
+++ app/src/main/java/com/example/quizzy/screens/FullScreenImageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.quizzy.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quizzy.R
+
+class FullScreenImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen_image)
+    }
+}
